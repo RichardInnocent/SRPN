4c78a2d3191729247c5c88d7af8c2c082e35173b
RichardInnocent <richardinnocent@sky.com>
2020-11-29 16:35:33 +0000

    Added tests and documentation

--- src/main/java/SubtractionOperator.java
+++ src/main/java/SubtractionOperator.java
@@ -1,7 +1,11 @@
+/**
+ * Operator to handle the subtraction of one operand from another.
+ */
 public class SubtractionOperator extends Operator {
 
   @Override
   public double calculate(double operand1, double operand2) {
+    // Perform the calculation and then make sure that it's still within the bounds on an integer
     return truncateToBounds(operand1 - operand2);
   }
 
--- src/test/java/SubtractionOperatorTest.java
+++ src/test/java/SubtractionOperatorTest.java
@@ -4,44 +4,49 @@ import org.junit.Test;
 
 public class SubtractionOperatorTest {
 
+  private static final double DELTA = 1e-8;
   private static final Operator OPERATOR = new SubtractionOperator();
 
   @Test
   public void testBasicSubtractionOfPositiveNumbers() {
     int value1 = 12;
     int value2 = 15;
-    assertEquals(value1 - value2, OPERATOR.calculate(value1, value2));
+    assertEquals(value1 - value2, OPERATOR.calculate(value1, value2), DELTA);
   }
 
   @Test
   public void testBasicSubtractionOfOnePositiveAndOneNegativeNumber() {
     int value1 = 12;
     int value2 = -15;
-    assertEquals(value1 - value2, OPERATOR.calculate(value1, value2));
+    assertEquals(value1 - value2, OPERATOR.calculate(value1, value2), DELTA);
   }
 
   @Test
   public void testBasicSubtractionOfOneNegativeAndOnePositiveNumber() {
     int value1 = -12;
     int value2 = 15;
-    assertEquals(value1 - value2, OPERATOR.calculate(value1, value2));
+    assertEquals(value1 - value2, OPERATOR.calculate(value1, value2), DELTA);
   }
 
   @Test
   public void testBasicSubtractionNegativeNumbers() {
     int value1 = -12;
     int value2 = -15;
-    assertEquals(value1 - value2, OPERATOR.calculate(value1, value2));
+    assertEquals(value1 - value2, OPERATOR.calculate(value1, value2), DELTA);
   }
 
   @Test
   public void testSubtractionOfLargePositiveValuesFromLargeNegativeNumberTruncates() {
-    assertEquals(Integer.MIN_VALUE, OPERATOR.calculate(Integer.MIN_VALUE, Integer.MAX_VALUE));
+    assertEquals(
+        Integer.MIN_VALUE, OPERATOR.calculate(Integer.MIN_VALUE, Integer.MAX_VALUE), DELTA
+    );
   }
 
   @Test
   public void testSubtractionOfLargeNegativeFromLargePositiveNumberTruncates() {
-    assertEquals(Integer.MAX_VALUE, OPERATOR.calculate(Integer.MAX_VALUE, Integer.MIN_VALUE));
+    assertEquals(
+        Integer.MAX_VALUE, OPERATOR.calculate(Integer.MAX_VALUE, Integer.MIN_VALUE), DELTA
+    );
   }
 
 }
