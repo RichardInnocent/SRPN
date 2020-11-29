b38e9c6e5596158c1950387b690a6235b0478e2e
RichardInnocent <richardinnocent@sky.com>
2020-11-29 14:49:10 +0000

    Added documentation

--- src/main/java/SizeRestrictedStack.java
+++ src/main/java/SizeRestrictedStack.java
@@ -23,9 +23,14 @@ public class SizeRestrictedStack<E> extends Stack<E> {
     this.maxSize = maxSize;
   }
 
+  /**
+   * Pops the top item off of the stack and returns this item.
+   * @return The item that was popped off of the stack.
+   * @throws StackUnderflowException Thrown if the stack is empty.
+   */
   @Override
   public synchronized E pop() throws StackUnderflowException {
-    if (size() < 1) {
+    if (isEmpty()) {
       throw new StackUnderflowException();
     }
     return super.pop();
@@ -49,6 +54,12 @@ public class SizeRestrictedStack<E> extends Stack<E> {
     super.addElement(item);
   }
 
+  /**
+   * Pushes the item onto the stack if the stack is not at its maximum capacity.
+   * @param item The item to push onto the stack.
+   * @return {@code true} if the item was pushed onto the stack, or {@code false} if this could not
+   * be done as the stack is at its maximum capacity.
+   */
   @Override
   public boolean add(E item) {
     if (atMaximumCapacity()) {
@@ -58,53 +69,94 @@ public class SizeRestrictedStack<E> extends Stack<E> {
     return true;
   }
 
+  /**
+   * Inserts the element at the given index.
+   * @param element The element to add.
+   * @param index The index to insert the index.
+   * @throws StackOverflowException Thrown if the stack is currently at its maximum capacity.
+   */
   @Override
   public void insertElementAt(E element, int index) throws StackOverflowException {
     verifyNotAtMaximumCapacity();
     super.insertElementAt(element, index);
   }
 
+  /**
+   * Pushes all elements in the collection onto the stack. If this is not possible as the maximum
+   * stack size would be exceeded, this operation has no effect.
+   * @param elements The elements to push onto the stack.
+   * @return {@code true} if the elements were successfully pushed onto the stack.
+   */
   @Override
   public boolean addAll(Collection<? extends E> elements) {
-    /* If the new size would be greater than capacity, return false and do nothing. Otherwise, add
-     * the elements */
     return canAddCollection(elements) && super.addAll(elements);
   }
 
+  /**
+   * Adds all elements in the collection into the stack at the given index. If this is not possible
+   * as the maximum stack size would be exceeded, this operation has no effect.
+   * @param index The index to at which to insert the elements.
+   * @param elements The elements to add.
+   * @return {@code true} if the elements were successfully added.
+   */
   @Override
   public synchronized boolean addAll(int index, Collection<? extends E> elements) {
-    /* If the new size would be greater than capacity, return false and do nothing. Otherwise, add
-     * the elements */
     return canAddCollection(elements) && super.addAll(index, elements);
   }
 
+  /**
+   * @throws StackOverflowException If this stack is at its maximum capacity.
+   */
   private void verifyNotAtMaximumCapacity() throws StackOverflowException {
     if (atMaximumCapacity()) {
       throw new StackOverflowException();
     }
   }
 
+  /**
+   * Determines whether the stack is at its maximum capacity.
+   * @return {@code true} if the stack is at its maximum capacity.
+   */
   public boolean atMaximumCapacity() {
     return size() >= maxSize;
   }
 
+  /**
+   * If the stack is not full, the given {@code action} is performed. If the stack is full, a
+   * {@link StackOverflowException} is thrown.
+   * @param action The action to perform if the stack is not full.
+   */
+  public void doIfNotFullOrThrowException(Consumer<SizeRestrictedStack<E>> action)
+      throws StackOverflowException {
+    if (atMaximumCapacity()) {
+      throw new StackOverflowException();
+    }
+    action.accept(this);
+  }
+
+  /**
+   * Determines if the stack can successfully push all elements of the other collection without
+   * exceeding its maximum capacity.
+   * @param collection The other collection to add.
+   * @return {@code true} if all elements of {@code collection} can be pushed onto {@code this}
+   * stack without exceeding the maximum capacity.
+   */
   private boolean canAddCollection(Collection<? extends E> collection) {
     return size() + collection.size() <= maxSize;
   }
 
+  /**
+   * This operation has no effect on this collection type.
+   * @param newSize The new size.
+   * @throws UnsupportedOperationException Thrown for all collections of this type. A
+   * {@code SizeRestrictedStack} has a fixed size.
+   */
   @Override
-  public synchronized void setSize(int newSize) {
+  public synchronized void setSize(int newSize) throws UnsupportedOperationException {
     /* Any throwing of this exception implies that we're breaking Liskov's Substitution Principle.
      * However, there are several other members of the Collection library that do this, e.g.
      * Collections.SingletonList. Being able to pass this object around as though it's part of the
      * standard Java Collections library is too convenient to ignore. */
     throw new UnsupportedOperationException("Size is fixed at initialisation");
-  }
-
-  public void doIfNotFullOrThrowException(Consumer<SizeRestrictedStack<E>> action) {
-    if (atMaximumCapacity()) {
-      throw new StackOverflowException();
-    }
-    action.accept(this);
   }
 }
