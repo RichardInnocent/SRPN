6dfbee32571923ed880dc6f60214014ff0b5f957
RichardInnocent <richardinnocent@sky.com>
2020-11-29 13:23:08 +0000

    Added tests and documentation

--- src/main/java/OperatorToken.java
+++ src/main/java/OperatorToken.java
@@ -1,23 +1,39 @@
+/**
+ * Abstract implementation of an operator token. This abstracts some of the complexities of how to
+ * handle the stack in each instance, allowing the calculation implementation to be handled purely
+ * by the {@link Operator}.
+ */
 public abstract class OperatorToken extends AbstractToken implements Comparable<OperatorToken> {
 
+  /**
+   * Gets the operator that is responsible for calculating the result.
+   * @return The operator that is responsible for calculating the result.
+   */
   public abstract Operator getOperator();
 
   @Override
   public void apply(SizeRestrictedStack<Double> operandStack) throws CalculatorException {
+    /* If the stack has fewer than 2 elements, throw an exception and don't pop any values off or
+     * attempt to calculate a value. */
     if (operandStack.size() < 2) {
       throw new StackUnderflowException();
     }
+
+    /* Get the operands. Don't want to pop these off of the stack until the calculation has
+     * completed successfully. */
     double operand1 = operandStack.get(operandStack.size() - 2);
     double operand2 = operandStack.get(operandStack.size() - 1);
 
+    // Get the result - this will throw an exception if unsuccessful
     double result = getOperator().calculate(operand1, operand2);
 
-    // If this works, we can pop both of the operands off the stack and then push on the result
+    // Generating the result was successful so pop the operands off the stack and push on the result
     operandStack.pop();
     operandStack.pop();
     operandStack.push(result);
   }
 
+  @Override
   public String getReadableValue() {
     return getOperator().getReadableName();
   }
@@ -40,8 +56,15 @@ public abstract class OperatorToken extends AbstractToken implements Comparable<
     return getOperator().equals(((OperatorToken) o).getOperator());
   }
 
+  /**
+   * Operators with a higher precedence should appear first in a sorted collection.
+   * @param other The operator to compare.
+   * @return A value {@code < 0} if this operator token has a higher precedence than {@code other},
+   * {@code > 0} if {@code other} has a higher precedence, or {@code 0} if the precedences are
+   * equal.
+   */
   @Override
-  public int compareTo(OperatorToken o) {
-    return getOperator().compareTo(o.getOperator());
+  public int compareTo(OperatorToken other) {
+    return getOperator().compareTo(other.getOperator());
   }
 }
--- src/test/java/OperatorTokenTest.java
+++ src/test/java/OperatorTokenTest.java
@@ -1,9 +1,60 @@
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 
+import java.util.function.BiFunction;
 import org.junit.Test;
 
 public class OperatorTokenTest {
+
+  private final OperatorToken token = createOperatorTokenFromOperator(Double::sum);
+
+  @Test(expected = StackUnderflowException.class)
+  public void testApplyThrowsUnderflowExceptionIfStackIsEmpty() {
+    SizeRestrictedStack<Double> operandStack = new SizeRestrictedStack<>(1);
+    token.apply(operandStack);
+  }
+
+  @Test(expected = StackUnderflowException.class)
+  public void testApplyThrowsUnderflowExceptionIfStackOnlyHasOneElement() {
+    SizeRestrictedStack<Double> operandStack = new SizeRestrictedStack<>(1);
+    operandStack.push(10.2);
+    token.apply(operandStack);
+  }
+
+  @Test
+  public void testSuccessfulApplication() {
+    SizeRestrictedStack<Double> operandStack = new SizeRestrictedStack<>(2);
+    double operand1 = 10.2;
+    double operand2 = 13.8;
+    operandStack.push(operand1);
+    operandStack.push(operand2);
+    token.apply(operandStack);
+    assertEquals(1, operandStack.size());
+    assertEquals(operand1 + operand2, operandStack.peek(), 1e-8);
+  }
+
+  @Test
+  public void testUnsuccessfulApplication() {
+    SizeRestrictedStack<Double> operandStack = new SizeRestrictedStack<>(2);
+    double operand1 = 10.2;
+    double operand2 = 13.8;
+    operandStack.push(operand1);
+    operandStack.push(operand2);
+
+    Token token =
+        createOperatorTokenFromOperator(
+            (op1, op2) -> {throw new CalculatorException("Test exception"){};}
+        );
+
+    try {
+      token.apply(operandStack);
+      fail("No exception thrown");
+    } catch (CalculatorException e) {
+      assertEquals(2, operandStack.size());
+      assertEquals(operand1, operandStack.get(0), 1e-8);
+      assertEquals(operand2, operandStack.get(1), 1e-8);
+    }
+  }
 
   @Test
   public void testHashCode() {
@@ -21,9 +72,27 @@ public class OperatorTokenTest {
     OperatorToken token2 = createOperatorTokenFromOperator(operator2);
 
     assertTrue(token1a.equals(token1b));
-    assertTrue(token1b.equals(token1a));
+    assertTrue(token1b.equals(token1a)); // ensure symmetric
     assertFalse(token1a.equals(token2));
-    assertFalse(token2.equals(token1a));
+    assertFalse(token2.equals(token1a)); // ensure symmetric
+  }
+
+  @Test
+  public void testCompareTo() {
+    OperatorToken highestPrecedence = createOperatorTokenWithPrecedence(2);
+    OperatorToken mediumPrecedence = createOperatorTokenWithPrecedence(1);
+    OperatorToken lowestPrecedence = createOperatorTokenWithPrecedence(0);
+
+    assertTrue(highestPrecedence.compareTo(mediumPrecedence) < 0);
+    assertTrue(mediumPrecedence.compareTo(highestPrecedence) > 0);
+
+    assertTrue(mediumPrecedence.compareTo(lowestPrecedence) < 0);
+    assertTrue(lowestPrecedence.compareTo(mediumPrecedence) > 0);
+  }
+
+  private static OperatorToken createOperatorTokenFromOperator(
+      BiFunction<Double, Double, Double> calculation) {
+    return createOperatorTokenFromOperator(createOperator(calculation));
   }
 
   private static OperatorToken createOperatorTokenFromOperator(Operator operator) {
@@ -31,6 +100,56 @@ public class OperatorTokenTest {
       @Override
       public Operator getOperator() {
         return operator;
+      }
+    };
+  }
+
+  private static Operator createOperator(BiFunction<Double, Double, Double> calculation) {
+    return new Operator() {
+      @Override
+      public double calculate(double operand1, double operand2) {
+        return calculation.apply(operand1, operand2);
+      }
+
+      @Override
+      public String getReadableName() {
+        return null;
+      }
+
+      @Override
+      public int getPrecedence() {
+        return 0;
+      }
+    };
+  }
+
+  private static OperatorToken createOperatorTokenWithPrecedence(int precedence) {
+    return new OperatorToken() {
+
+      private final Operator operator = createOperatorWithPrecedence(precedence);
+
+      @Override
+      public Operator getOperator() {
+        return operator;
+      }
+    };
+  }
+
+  private static Operator createOperatorWithPrecedence(int precedence) {
+    return new Operator() {
+      @Override
+      public double calculate(double operand1, double operand2) {
+        return 0;
+      }
+
+      @Override
+      public String getReadableName() {
+        return "Precedence " + precedence;
+      }
+
+      @Override
+      public int getPrecedence() {
+        return precedence;
       }
     };
   }
